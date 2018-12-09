4dc68f4c4401aee18c1e215138149413cb793509
Junior Ignacio <jigjunior@gmail.com>
2018-12-09 19:24:18 -0200

    2018-12-09 - Commit - 1 - Modelagem de layout - Preparação para 2 Activities independentes, afim de reduzir a complexidade da Arquitetura em estudo

---
+++ app/src/main/java/com/avanade/santander/testeandroid/contato/ContatoActivity.java
@@ -0,0 +1,22 @@
+package com.avanade.santander.testeandroid.contato;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.avanade.santander.testeandroid.R;
+
+public class ContatoActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contato);
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/avanade/santander/testeandroid/fundo/Fundos.java
@@ -0,0 +1,15 @@
+package com.avanade.santander.testeandroid.fundo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.avanade.santander.testeandroid.R;
+
+public class Fundos extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fundos);
+    }
+}
